58d044649d379a65eb58818596b453af7cfe9d35
unknown <mahmud.islam32@gmail.com>
2021-05-24 02:24:41 +0200

    setting up Twilwind Css

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div>
+       <Image
+        src="/me.png"
+        width={50}
+        height={50}
+      />
+        </div>
+    )
+}
+
+export default Header
