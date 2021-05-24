ae759dcad61b04bf8f17208f7704ee0c737cbc18
unknown <mahmud.islam32@gmail.com>
2021-05-24 23:47:09 +0200

    Header Created

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,61 @@
+import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
+
 function Header() {
     return (
-        <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
        <Image
-        src="/me.png"
-        width={50}
-        height={50}
+        src="https://links.papareact.com/5me"
+        width={40}
+        height={40}
+        layout="fixed"
       />
+       <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+           <SearchIcon className="h-6 text-gray-600"/>
+           
+           <input className="hidden md:inline-flex items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search on Facebook" />
+       </div>
+
+       {/* Center */}
+
+        <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+        <HeaderIcon active Icon={HomeIcon} />
+        <HeaderIcon Icon={PlayIcon} />
+        <HeaderIcon Icon={FlagIcon} />
+        <HeaderIcon Icon={ShoppingCartIcon} />
+        <HeaderIcon Icon={UserGroupIcon} />
         </div>
+        
+        
+        
+        </div>
+        
+        <div className="flex items-center justify-end sm:space-x-2">
+            <p className=" whitespace-nowrap pr-3 font-semibold">Mahmudul Islam</p>
+            <ViewGridIcon className="icon" />
+            <ChatIcon className="icon" />
+            <BellIcon className="icon" />
+            <ChevronDownIcon className="icon" />
+        </div>
+      
+
+        </div>
+        
     )
 }
 
--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ module.exports = {
  
     images:{
         domains:[
-            
+          "links.papareact.com",
+          "platform-lookaside.fbsbx.com",
+          "firebasestorage.googleapis.com"  
         ]
     }
     
