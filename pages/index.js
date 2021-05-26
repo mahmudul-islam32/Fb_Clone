55ec02cd84b306109646a1e96816abdacdaa5fde
unknown <mahmud.islam32@gmail.com>
2021-05-27 00:51:43 +0200

    integrate Autentication

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
-export default function Home() {
+import Login from "../components/Login";
+export default function Home({ session }) {
+
+  if(!session) return <Login />
   return (
     <div>
       <Head>
@@ -9,4 +13,16 @@ export default function Home() {
       <Header />
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
+     
+
 }
